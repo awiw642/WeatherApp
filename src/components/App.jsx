d381df28f84d42b3f2fba989744da2964d8da8dc
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-06 11:51:06 +1300

    Create weather result component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SearchBar from './SearchBar.jsx';
+import WeatherResult from './WeatherResult.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherResult />
       </div>
     );
   }
--- src/components/WeatherResult.jsx
+++ src/components/WeatherResult.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
 
-export default class WeatherResult extends React.Component {
-  
+import { updateLocationValue, getWeather, getWeatherForecast } from '../actionCreators';
+
+const WeatherResultWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  margin-top: 1em;
+  justify-items: center;
+`;
+class WeatherResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.initialiseLocationWeather = this.initialiseLocationWeather.bind(this);
+  }
+  componentDidMount() {
+    this.initialiseLocationWeather();
+  }
+
+  initialiseLocationWeather() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      const geolocation = { lat: latitude, long: longitude };
+      this.props.updateLocation(geolocation)
+      this.props.getWeatherOnLocation(geolocation);
+      this.props.getWeatherForecastOnLocation(geolocation);
+    });
+  }
+
+  render() {
+    return (
+      <WeatherResultWrapper>
+        <h1>test</h1>
+        
+      </WeatherResultWrapper>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  currentWeather: state.currentWeather,
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateLocation(geolocation) {
+    dispatch(updateLocationValue(geolocation));
+  },
+  getWeatherOnLocation(geolocation) {
+    dispatch(getWeather(geolocation));
+  },
+  getWeatherForecastOnLocation(geolocation) {
+    dispatch(getWeatherForecast(geolocation));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherResult);
