aba8d45b9b659440672efe7346af7d6d76d92d04
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-08 09:39:20 +1300

    Create front-end routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SearchBar from './SearchBar.jsx';
 import WeatherResult from './WeatherResult.jsx';
+import Navigation from './Navigation.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <SearchBar />
-        <WeatherResult />
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={SearchBar} />
+            <Route path="/result" component={WeatherResult} />
+            <Route path="/search" component={SearchBar} />
+            <Route
+              render={() => {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
